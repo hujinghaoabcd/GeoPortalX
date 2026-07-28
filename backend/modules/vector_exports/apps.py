6f4e26a97d75b559c756a2516578a33c5ebdf634
Jinghao Hu <731409213@qq.com>
2026-07-28 21:13:53 +0800

    Configure vector export app

---
+++ backend/modules/vector_exports/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VectorExportsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "modules.vector_exports"
+    label = "vector_exports"
