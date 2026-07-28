4e75c0b753d2433fd9c7b1c1422afeb0deabeac6
Jinghao Hu <731409213@qq.com>
2026-07-28 22:03:47 +0800

    Mount vector style API

--- backend/geoportalx/api.py
+++ backend/geoportalx/api.py
@@ -9,6 +9,7 @@ from modules.organizations.api import router as organizations_router
 from modules.resources.api import router as resources_router
 from modules.uploads.api import router as uploads_router
 from modules.vector_exports.api import router as vector_exports_router
+from modules.vector_styles.api import router as vector_styles_router
 from modules.vector_tiles.api import router as vector_tiles_router
 
 api = NinjaAPI(title="GeoPortalX API", version="1.0.0", urls_namespace="api-v1")
@@ -40,4 +41,5 @@ api.add_router("/jobs", jobs_router)
 api.add_router("/uploads", uploads_router)
 api.add_router("/datasets", datasets_router)
 api.add_router("/vector-layers", vector_tiles_router)
+api.add_router("/vector-layers", vector_styles_router)
 api.add_router("/vector-exports", vector_exports_router)
