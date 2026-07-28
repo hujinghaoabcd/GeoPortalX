6c2db34178e4f751b4485a545269fb50a7c60d58
Jinghao Hu <731409213@qq.com>
2026-07-29 01:40:50 +0800

    Route raster preview to isolated root component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,10 @@ import { createApp } from 'vue';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 import App from './App.vue';
+import RasterApp from './RasterApp.vue';
 import './style.css';
 
-createApp(App).use(createPinia()).mount('#app');
+const query = new URLSearchParams(window.location.search);
+const Root = query.has('rasterDataset') ? RasterApp : App;
+
+createApp(Root).use(createPinia()).mount('#app');
